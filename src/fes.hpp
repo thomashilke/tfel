b1a63c879272962b385f1cf4c7d148f90a9f0b6e
Thomas Foetisch <thomas.foetisch@epfl.ch>
2017-06-13 11:22:01 +0200

    Declaration of the finite element space template

---
+++ src/fes.hpp
@@ -0,0 +1,18 @@
+#ifndef _FES_H_
+#define _FES_H_
+
+#include "mesh.hpp"
+
+template<typename fe>
+class finite_element_space {
+public:
+  typedef fe fe_type;
+  typedef typename fe_type::cell_type cell_type;
+
+  finite_element_space(const mesh<cell_type>& m);
+
+private:
+  array<unsigned int> dof_map;
+};
+
+#endif /* _FES_H_ */

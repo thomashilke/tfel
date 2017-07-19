8eb113ba9c27da3c08bd9ead2a79f50f3c22cf0b
Thomas Foetisch <thomas.foetisch@epfl.ch>
2017-07-19 11:42:58 +0200

    Define the triangle lagrange p1 bubble finite element

--- src/fe.hpp
+++ src/fe.hpp
@@ -258,6 +258,17 @@ namespace finite_element {
   };
 
   struct triangle_lagrange_p1_bubble: public triangle_lagrange_p1 {
+    using triangle_lagrange_p1::bf_0_1;
+    using triangle_lagrange_p1::bf_0_2;
+    using triangle_lagrange_p1::bf_0_3;
+    using triangle_lagrange_p1::bf_10_1;
+    using triangle_lagrange_p1::bf_10_2;
+    using triangle_lagrange_p1::bf_10_3;
+    using triangle_lagrange_p1::bf_01_1;
+    using triangle_lagrange_p1::bf_01_2;
+    using triangle_lagrange_p1::bf_01_3;
+    
+    
     using triangle_lagrange_p1::cell_type;
 
     static const std::size_t n_dof_per_element = 4;
@@ -266,12 +277,51 @@ namespace finite_element {
       return n_dof[i];
     }
     static const bool is_continuous = true;
+    static const bool is_lagrangian = false;
 
+    static constexpr double x[4][2] = {{}, {}, {}, {}};
+    
     static double basis_function(unsigned int i,
-				 unsigned int* derivative,
-				 double* x) {
-      return 0.0;
+				 const unsigned int* derivative,
+				 const double* x) {
+      typedef double(*bf_type)(const double*);
+      static const bf_type bf[3][4] = {{bf_0_1, bf_0_2, bf_0_3, bf_0_4},
+				       {bf_10_1, bf_10_2, bf_10_3, bf_10_4},
+				       {bf_01_1, bf_01_2, bf_01_3, bf_01_4}};
+
+      if (derivative[0] == 0 and derivative[1] == 0) {
+	return bf[0][i](x);
+      } else if (derivative[0] == 1 and derivative[1] == 0) {
+	return bf[1][i](x);;
+      } else if (derivative[0] == 0 and derivative[1] == 1) {
+	return bf[1][i](x);;
+      } else {
+	throw std::string("triangle_lagrange_p1_bubble: unsupported derivative order.");
+      }
     }
+
+    static double phi(unsigned int i, const double* x) {
+      unsigned int d[2] = {0, 0};
+      return basis_function(i, d, x);
+    }
+
+    static double dphi(unsigned int k, unsigned int i, const double* x) {
+      if (k >= 2)
+	throw std::string("finite_element::triangle_lagrange_p1::dphi: space dimension is 2,"
+			  " therefore k must be in {0, 1}.");
+      unsigned int d[2] = {0, 0};
+      d[k] = 1;
+      return basis_function(i, d, x);
+    }
+
+  protected:
+    static double bf_0_4(const double* x) { return bf_0_1(x) * bf_0_2(x) * bf_0_3(x); }
+    static double bf_10_4(const double* x) { return (bf_10_1(x) * bf_0_2(x) * bf_0_3(x)
+						     + bf_0_1(x) * bf_10_2(x) * bf_0_3(x)
+						     + bf_0_1(x) * bf_0_2(x) * bf_10_3(x)); }
+    static double bf_01_4(const double* x) { return (bf_01_1(x) * bf_0_2(x) * bf_0_3(x)
+						     + bf_0_1(x) * bf_01_2(x) * bf_0_3(x)
+						     + bf_0_1(x) * bf_0_2(x) * bf_01_3(x)); }
   };
 }
 

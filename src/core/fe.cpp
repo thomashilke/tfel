5bba0f41aea44404f7714ef2e877a0cd8d607129
Thomas Foetisch <thomas.foetisch@epfl.ch>
2017-07-26 17:39:25 +0200

    Tetrahedron lagrange p0 and p1 finite element, Tetrahedron cell implementation skeleton

--- src/core/fe.cpp
+++ src/core/fe.cpp
@@ -4,11 +4,21 @@ constexpr std::size_t finite_element::edge_lagrange_p0::n_dof[2];
 constexpr std::size_t finite_element::edge_lagrange_p1::n_dof[2];
 constexpr std::size_t finite_element::edge_lagrange_p1_bubble::n_dof[2];
 
+constexpr std::size_t finite_element::triangle_lagrange_p0::n_dof[3];
+constexpr std::size_t finite_element::triangle_lagrange_p1::n_dof[3];
+constexpr std::size_t finite_element::triangle_lagrange_p1_bubble::n_dof[3];
+
+constexpr std::size_t finite_element::tetrahedron_lagrange_p0::n_dof[4];
+constexpr std::size_t finite_element::tetrahedron_lagrange_p1::n_dof[4];
+
+
 constexpr double finite_element::edge_lagrange_p0::x[1][1];
 constexpr double finite_element::edge_lagrange_p1::x[2][1];
 constexpr double finite_element::edge_lagrange_p1_bubble::x[3][1];
-constexpr double finite_element::triangle_lagrange_p0::x[1][2];
-constexpr std::size_t finite_element::triangle_lagrange_p0::n_dof[3];
 
+constexpr double finite_element::triangle_lagrange_p0::x[1][2];
 constexpr double finite_element::triangle_lagrange_p1::x[3][2];
 constexpr double finite_element::triangle_lagrange_p1_bubble::x[4][2];
+
+constexpr double finite_element::tetrahedron_lagrange_p0::x[1][3];
+constexpr double finite_element::tetrahedron_lagrange_p1::x[4][3];
--- src/core/fe.hpp
+++ src/core/fe.hpp
@@ -111,7 +111,7 @@ namespace finite_element {
     }
     static const bool is_continuous = true;
     static const bool is_lagrangian = false;
-
+    
     static constexpr double x[3][1] = {{0.0}, {1.0}, {0.5}};
     
     static double basis_function(unsigned int i,
@@ -173,6 +173,7 @@ namespace finite_element {
     }
     static const bool is_continuous = false;
     static const bool is_lagrangian = true;
+    
     static constexpr double x[1][2] = {{1.0 / 3.0, 1.0 / 3.0}};
 
     static double basis_function(unsigned int i,
@@ -202,8 +203,8 @@ namespace finite_element {
     typedef cell::triangle cell_type;
 
     static const std::size_t n_dof_per_element = 3;
-    static std::size_t n_dof_per_subdomain(unsigned int i) {
-      static const std::size_t n_dof[] = {1, 0, 0};
+    static constexpr std::size_t n_dof[] = {1, 0, 0};
+    static constexpr std::size_t n_dof_per_subdomain(unsigned int i) {
       return n_dof[i];
     }
     static const bool is_continuous = true;
@@ -272,8 +273,8 @@ namespace finite_element {
     using triangle_lagrange_p1::cell_type;
 
     static const std::size_t n_dof_per_element = 4;
-    static std::size_t n_dof_per_subdomain(unsigned int i) {
-      static const std::size_t n_dof[] = {1, 0, 1};
+    static constexpr std::size_t n_dof[] = {1, 0, 1};
+    static constexpr std::size_t n_dof_per_subdomain(unsigned int i) {
       return n_dof[i];
     }
     static const bool is_continuous = true;
@@ -325,6 +326,111 @@ namespace finite_element {
     static double bf_01_4(const double* x) { return 27.0 * (bf_01_1(x) * bf_0_2(x) * bf_0_3(x)
 							    + bf_0_1(x) * bf_01_2(x) * bf_0_3(x)
 							    + bf_0_1(x) * bf_0_2(x) * bf_01_3(x)); }
+  };
+
+  struct tetrahedron_lagrange_p0 {
+    typedef cell::tetrahedron cell_type;
+
+    static const std::size_t n_dof_per_element = 1;
+    static constexpr std::size_t n_dof[] = {0, 0, 0, 1};
+    static constexpr std::size_t n_dof_per_subdomain(unsigned int i) {
+      return n_dof[i];
+    }
+    static const bool is_continuous = false;
+    static const bool is_lagrangian = true;
+
+    static constexpr double x[1][3] = {{1.0/4.0, 1.0/4.0, 1.0/4.0}};
+
+    static double basis_function(unsigned int i,
+				 unsigned int* derivatives,
+				 const double* x) {
+      if (derivatives[0] == 0 and derivatives[1] == 0 and derivatives[2] == 0) {
+	return 1.0;
+      } else {
+	return 0.0;
+      }
+    }
+
+    static double phi(unsigned int i, const double* x) {
+      return 1.0;
+    }
+      
+    static double dphi(unsigned int k, unsigned int i, const double* x) {
+      if (k >= 3)
+	throw std::string("finite_element::edge_lagrange_p0::dphi: space dimension is 1,"
+			  " therefore k must be in {0, 1}.");
+      return 0.0;
+    }
+  };
+
+  struct tetrahedron_lagrange_p1 {
+    typedef cell::tetrahedron cell_type;
+
+    static const std::size_t n_dof_per_element = 4;
+    static constexpr std::size_t n_dof[] = {1, 0, 0, 0};
+    static constexpr std::size_t n_dof_per_subdomain(unsigned int i) {
+      return n_dof[i];
+    }
+    static const bool is_continuous = true;
+    static const bool is_lagrangian = true;
+
+    static constexpr double x[4][3] = {{0.0, 0.0, 0.0},
+				       {1.0, 0.0, 0.0},
+				       {0.0, 1.0, 0.0},
+				       {0.0, 0.0, 1.0}};
+
+    static double basis_function(unsigned int i,
+				 const unsigned int* derivative,
+				 const double* x) {
+      typedef double (*bf_type)(const double*);
+      static const bf_type bf[4][4] = {{bf_000_1, bf_000_2, bf_000_3, bf_000_4},
+				       {bf_100_1, bf_100_2, bf_100_3, bf_100_4},
+				       {bf_010_1, bf_010_2, bf_010_3, bf_010_4},
+				       {bf_001_1, bf_001_2, bf_001_3, bf_001_4}};
+      if (derivative[0] == 0 and derivative[1] == 0 and derivative[2] == 0) {
+	return bf[0][i](x);
+      } else if(derivative[0] == 1 and  derivative[1] == 0 and derivative[2] == 0) {
+	return bf[1][i](x);
+      } else if (derivative[0] == 0 and  derivative[1] == 1 and derivative[2] == 0) {
+	return bf[2][i](x);
+      } else if (derivative[0] == 0 and  derivative[1] == 0 and derivative[2] == 1) {
+	return bf[3][i](x);
+      } else {
+	return 0.0;
+      }
+    }
+
+    static double phi(unsigned int i, const double* x) {
+      unsigned int d[3] = {0, 0, 0};
+      return basis_function(i, d, x);
+    }
+
+    static double dphi(unsigned int k, unsigned int i, const double* x) {
+      if (k >= 3)
+	throw std::string("finite_element::tetrahedron_lagrange_p1::dphi: space dimension is 3,"
+			  " therefore k must be in {0, 2}.");
+      unsigned int d[3] = {0, 0, 0};
+      d[k] = 1;
+      return basis_function(i, d, x);
+    }
+
+  protected:
+    static double bf_000_1(const double* x) { return 1.0 - x[0] - x[1] - x[2]; }
+    static double bf_000_2(const double* x) { return x[0]; }
+    static double bf_000_3(const double* x) { return x[1]; }
+    static double bf_000_4(const double* x) { return x[2]; }
+    static double bf_100_1(const double* x) { return -1.0; }
+    static double bf_100_2(const double* x) { return  1.0; }
+    static double bf_100_3(const double* x) { return  0.0; }
+    static double bf_100_4(const double* x) { return  0.0; }
+    static double bf_010_1(const double* x) { return -1.0; }
+    static double bf_010_2(const double* x) { return  0.0; }
+    static double bf_010_3(const double* x) { return  1.0; }
+    static double bf_010_4(const double* x) { return  0.0; }
+    static double bf_001_1(const double* x) { return -1.0; }
+    static double bf_001_2(const double* x) { return  0.0; }
+    static double bf_001_3(const double* x) { return  0.0; }
+    static double bf_001_4(const double* x) { return  1.0; }
   };
 }
 
